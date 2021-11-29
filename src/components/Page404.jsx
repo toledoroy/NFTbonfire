af63f656d6d250cc03f8ba05b7aa4c0c86dbe34a
Roy <toledoroy@gmail.com>
2021-11-29 10:37:57 -0800

    Added 404 Page
    - Cleaned Footer
    - Sorted Routes

---
+++ src/components/Page404.jsx
@@ -0,0 +1,13 @@
+/**
+ * 404 Page
+ */
+function Page404(props){
+    return (
+      <div>
+        <h1>404</h1>
+        <p>Page not found</p>
+      </div>
+    )
+  }//Page404()
+
+export default Page404;
