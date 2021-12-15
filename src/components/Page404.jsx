b14c030ab38b7e997fd8b563e288f531f35c473f
Roy <toledoroy@gmail.com>
2021-12-15 14:51:50 -0800

    Updated 404 Page

--- src/components/Page404.jsx
+++ src/components/Page404.jsx
@@ -1,13 +1,20 @@
+import { Result, Button } from 'antd';
+import { Link } from "react-router-dom";
+
 /**
  * 404 Page
  */
 function Page404(props){
-    return (
-      <div>
-        <h1>404</h1>
-        <p>Page not found</p>
-      </div>
-    )
+  return (<Result status="404" title="404" subTitle="Sorry, the page you're looking for does not exist." extra={
+    <Link to={{pathname:'/'}}><Button type="primary">Back Home</Button></Link>
+  } />);
+
+    // return (
+    //   <div>
+    //     <h1>404</h1>
+    //     <p>Page not found</p>
+    //   </div>
+    // )
   }//Page404()
 
 export default Page404;
