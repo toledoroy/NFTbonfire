751a65a1556250b8e86963798a4da5d120eed9c7
Roy <toledoroy@gmail.com>
2021-12-24 15:54:55 -0800

    [TEST] DB Scema

--- cloud/cloud_db.js
+++ cloud/cloud_db.js
@@ -8,7 +8,7 @@
 //     if (notifyMonsterGuild) {
 //       // Notify the guild about new monster.
 //     }
-//   });
+// });
   
 
 // Moralis.Cloud.beforeSave('_User', request => {
@@ -16,5 +16,27 @@
 //     if (!user.get("email")) {
 //       throw "Every user must have an email address.";
 //     }
-//   });
+// });
   
+Moralis.Cloud.beforeSave('dbSchema', request => {
+    
+    //TODO: Test This
+
+    //[DEV] Schema Stuff        https://parseplatform.org/Parse-SDK-JS/api/2.9.0/Parse.Schema.html
+    const schema = new Moralis.Schema('Persona');
+    console.warn("[DEV] Moralis Persona Schema:", schema);
+    // const options = { required: true, defaultValue: 'hello world' };
+    // schema.addString('TestField', options);
+    schema.addIndex('i_handle', { 'handle': 1 });
+    // schema.save();
+    schema.save(null, {useMasterKey: true});
+
+    /* TODO: Edit Schema
+    const schema = new Moralis.Schema('Post');
+    console.warn("[DEV] Moralis Schema:", schema);
+    const options = { required: true, defaultValue: 'hello world' };
+    schema.addString('TestField', options);
+    schema.addIndex('index_name', { 'TestField': 1 });
+    schema.save();
+    */
+});
