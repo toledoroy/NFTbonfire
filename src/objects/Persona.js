b414823e5de4b3ddfe2bf38adc420168e89d9e8a
Roy <toledoroy@gmail.com>
2021-12-23 20:25:14 -0800

    Cleanup

--- src/objects/Persona.js
+++ src/objects/Persona.js
@@ -1,160 +1,10 @@
-import { IPFS } from "helpers/IPFS";
-import { PersonaHelper } from "helpers/PersonaHelper";
-
 const Moralis = require("moralis/node");
-const APP_ID = process.env.REACT_APP_MORALIS_APPLICATION_ID;
-const SERVER_URL = process.env.REACT_APP_MORALIS_SERVER_URL;
-Moralis.initialize(APP_ID);
-Moralis.serverURL = SERVER_URL;
 
-
-
-//-- Persona
+/**
+ * Object: Persona
+ */ 
 export const Persona = Moralis.Object.extend("Persona", 
     { /* Instance Methods */
-        
-        //Override Save
-        // save(){
-            // console.warn("[TEST] save() Persona Override for Save Function");
-        //     this.saveToChain();
-            // return super.save();
-        // },
-        // saveToChain(){
-        //     console.warn("[TEST] saveToChain() Called");
-        // },
-
-        //-- Loaders
-        /**
-         * Load Metadata from Chain
-         * @ret object / null       Metadata or Default Metadata (or null)
-         * /
-         async loadMetadata(returnDefault=true){
-            if(this.get('token_id') !== undefined) return await this.updateToken();
-            else{ 
-                //-- New Persona, Not yet on Chain
-                //Random 
-                if(returnDefault){
-                    let defaultMetadata = Persona.getDefaultMetadata(); //return personaDefaultMetadata[Math.floor(Math.random()*personaDefaultMetadata.length)];
-                    console.warn("[TEST] Persona.loadMetadata() Return Default Metadata:", defaultMetadata)
-                    return defaultMetadata;
-                }
-                return null;
-                
-                // return returnDefault ? personaDefaultMetadata : null;
-            }
-        },
-        /**
-         * Update Token (Fetch from Chain -- URI & Metadata )
-         * @returns object metadata
-         * /
-        async updateToken(){
-            let chain = this.get("chain") || this.get("chainId");
-            // console.log("[TEST] Persona.updateToken()  Address:", this.get('address'), Persona.getContractAddress(chain), chain, this);
-            // return;
-            
-            //TODO: Validate Current Chain Matches Contract's Chain
-
-            //W3 - Fetch Token URI
-            let options = {
-                contractAddress: Persona.getContractAddress(chain),
-                params: { tokenId:this.get('token_id') },
-                functionName: "tokenURI",
-                abi:[{
-                    "name": "tokenURI",
-                    "type": "function",
-                    "stateMutability": "view",
-                    "inputs": [
-                        {
-                            "internalType": "uint256",
-                            "name": "tokenId",
-                            "type": "uint256"
-                        }
-                    ],
-                    "outputs": [
-                        {
-                            "internalType": "string",
-                            "name": "",
-                            "type": "string"
-                        }
-                    ],
-                }],
-            };
-            
-            try{
-                //Fetch Token URI
-                let uri = await Moralis.executeFunction(options);
-                
-                //Compare & Update Metadata if Needed
-                // if(this.get('token_uri') !== uri) {      //TODO: Enable
-                if(1 || this.get('token_uri') !== uri) {    //Always Load New Metadata
-                    //Log
-                    console.log("Persona.updateToken() Updating Token URI", {before:this.get('token_uri'), after:uri, perosna:this})
-                    //Update NFT
-                    this.set('token_uri', uri);
-                    //Update Metadata
-                    return await this.fetchMetadata(uri);
-                    
-                }//Different URI
-                else{
-                    //Log
-                    console.log("Persona.updateToken() Metadata URI is Up to Date -- Return Saved Metadata", {persona:this});
-                    return this.get('metadata');
-                } 
-            }
-            catch(error){
-                //Log
-               console.error("[TEST] Persona.updateToken() Error", {perosna:this, options, error});
-            }
-        },//updateToken()
-
-        /**
-         * Extract Metadata from NFT, 
-         *  Fallback: Fetch from URI
-         * @returns object metadata
-         * /
-         async fetchMetadata(token_uri) {
-            // const token_uri = this.get('token_uri');
-            //Validate URI
-            if(!token_uri || !token_uri.includes('://')){
-                console.log('Persona.fetchMetadata() Invalid URI', {URI: token_uri});
-                return;
-            }
-            //Get Metadata
-            let uri = IPFS.resolveLink(token_uri);
-            //Log
-            // console.log('Persona.fetchMetadata() Running With ', {token_uri, uri});
-            try{
-                let metadata = await fetch(uri).then(res => res.json());
-                if(!metadata){
-                    //Log
-                    console.error("Persona.fetchMetadata() No Metadata found on URI:", {uri});
-                }
-                //Handle Setbacks
-                else if(metadata?.detail  && metadata.detail.includes("Request was throttled")){
-                    //Log
-                    console.warn("Persona.fetchMetadata() Bad Result for:"+uri+"  Will retry later", {metadata});
-                    //Retry That Again after 1s
-                    // setTimeout(function() { this.fetchMetadata(uri); }, 1000);
-                    await new Promise(resolve => setTimeout(resolve, 1000));        //WAIT 1s
-                    //Run Again
-                    return await this.fetchMetadata(uri);
-                }//Handle Opensea's {detail: "Request was throttled. Expected available in 1 second."}
-                else{//No Errors
-                    //Set
-                    this.set('metadata', metadata);
-                    //Log
-                    console.log("Persona.fetchMetadata() New Metadata From"+uri, {persona:this, metadata});
-                    //Return Metadata
-                    return metadata;
-                }//Valid Result
-            }
-            catch(error){
-                //Log
-               console.error("[TEST] Persona.updateToken() Error", {perosna:this, error});
-            }
-        },//fetchMetadata()
-
-        //-- View
         /**
          * Get Requested Property from Metadata & Fallback to Defaults if Not Found
          * @returns 
@@ -184,12 +34,11 @@ export const Persona = Moralis.Object.extend("Persona",
         getContractAddress(chain){ return (this.contractPersona[chain]?.address) ? this.contractPersona[chain].address : null; },
         //Default Metadata (Random)
         getDefaultMetadata(){  return personaDefaultMetadatas[Math.floor(Math.random()*personaDefaultMetadatas.length)]; },
-        
     }
-);     //Personas
+); //Personas
 
+//-- ASSETS
 
-//** ASSETS **/
 //Default Persona Details
 const personaDefaultMetadatas = [
     /* Satoshi
