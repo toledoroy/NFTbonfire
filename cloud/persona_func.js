bca6ed1de77d8bc14073714dbe2a8fc29f2df4a3
Roy <toledoroy@gmail.com>
2021-12-27 17:34:46 -0800

    Added Persona Func: Register & UnRegister

--- cloud/persona_func.js
+++ cloud/persona_func.js
@@ -2,7 +2,10 @@
 
 // const logger = Moralis.Cloud.getLogger();
 
-//-- PRODUCTION
+const Persona = Moralis.Object.extend("Persona");
+const Contract = Moralis.Object.extend("Contract");
+
+//-- Helper Functions
 
 /**
  * Check if User Owns NFT
@@ -10,24 +13,98 @@
  * @param {*} hash   NFTs Hash
  * @returns 
  */
-const isHandleFree = async (handle) => {
+ const isHandleFree = async (handle) => {
   const reserved = ['persona','account', '...'];
   if(reserved.includes(handle.toLowerCase())) return false;
 
+  // const Persona = Moralis.Object.extend("Persona");
+  
   //Check if Handle Already Exists in DB
-  const Persona = Moralis.Object.extend("Persona");
   const query = new Moralis.Query(Persona);
   query.equalTo("handle", handle.toLowerCase());
   const results = await query.find();
 
-  logger.warn("[TEST] Found "+results.length+" Results for Requested Handle:"+handle+" Results: " + JSON.stringify(results));
+  logger.warn("[TEST] isHandleFree() Found "+results.length+" Results for Handle:"+handle+"");
+  // logger.warn("[TEST] isHandleFree() Found "+results.length+" Results for Handle:"+handle+" Results: " + JSON.stringify(results));
 
   //True if Empty
   return (results.length == 0);
 };//isHandleFree()
   
+/**
+ * Get ABI For Contract (From DB)
+ * @param {*} chain     Chain ID
+ * @param {*} address   Contract Address
+ * @returns array       Contract's ABI
+ */
+ const getABI = async (chain, address) => {
+  const query = new Moralis.Query(Contract);
+  query.equalTo("chain", chain);
+  query.equalTo("address", address);
+  const results = await query.limit(1).find();
+  // logger.warn("[TEST] getABI() Found "+results.length+" Results for Contract:"+address+" Chain:"+chain+"");  //V
+  if(results.length === 0) return null;
+  else return results[0].get('abi');
+};//getABI()
+  
+/**
+ * Get the Owner of a Token
+ * @param {*} chain 
+ * @param {*} contract 
+ * @param {*} tokenId 
+ */
+const getTokenOwner = async (chain, contract, tokenId) => {
+  //Fetch ABI for Contract
+  let abi = await getABI(chain, contract);
+  //Validate
+  if(!abi) throw new Error("Missing ABI for Contract:"+contract+" on Chain:"+chain);
+  let options = {
+    abi: JSON.parse(abi),
+    address: contract,
+    // function_name: 'tokenURI',
+    function_name: 'ownerOf',
+    params: { tokenId },
+    chain,
+  };
+  //Log
+  // logger.warn("[TEST] getTokenOwner() Run W3 Request:" + JSON.stringify(options) );  //V
+  //Run W3 Request
+  let res = await Moralis.Web3API.native.runContractFunction(options);
+  //Validate
+  if(!res) throw new Error("getTokenOwner() Failed to Fetch Persona res("+res+") from Chain:'"+chain+"' tokenId:'"+tokenId+"' contract:'"+contract+"'");
+  //Log
+  // logger.warn("[TEST] getTokenOwner() Request Params: chain:'"+chain+"' tokenId:'"+tokenId+"' contract:'"+contract+"' Ret:" + JSON.stringify(res) );  //V
+  return res;
+};
 
-const Persona = Moralis.Object.extend("Persona");
+/**
+ * Get the Owner of a Token
+ * @param {*} chain 
+ * @param {*} contract 
+ * @param {*} tokenId 
+ */
+ const getTokenURI = async (chain, contract, tokenId) => {
+  //Fetch ABI for Contract
+  let abi = await getABI(chain, contract);
+  //Validate
+  if(!abi) throw new Error("Missing ABI for Contract:"+contract+" on Chain:"+chain);
+  let options = {
+    abi: JSON.parse(abi),
+    address: contract,
+    function_name: 'tokenURI',
+    params: { tokenId },
+    chain,
+  };
+  //Log
+  // logger.warn("[TEST] getTokenURI() Run W3 Request:" + JSON.stringify(options) );  //V
+  //Run W3 Request
+  let res = await Moralis.Web3API.native.runContractFunction(options);
+  //Validate
+  if(!res) throw new Error("getTokenURI() Failed to Fetch Persona res("+res+") from Chain:'"+chain+"' tokenId:'"+tokenId+"' contract:'"+contract+"'");
+  //Log
+  // logger.warn("[TEST] getTokenURI() Request Params: chain:'"+chain+"' tokenId:'"+tokenId+"' contract:'"+contract+"' Ret:" + JSON.stringify(res) );  //V
+  return res;
+};
 
 /**
  * Fetch Persona from Chain & Save to DB
@@ -37,19 +114,12 @@ const Persona = Moralis.Object.extend("Persona");
  * @returns ParseObject
  */
 const cachePersona = async (chain, contract, tokenId) => {
-    // address = Persona.getContractAddress(chain)
-    let options = {
-      abi: Persona.getABI(chain),
-      address: contract,
-      function_name: 'tokenURI',
-      params: { tokenId },
-      chain,
-  };
-  let res = await Moralis.Web3API.native.runContractFunction(options);   //Through Server (All Chains)
   //Validate
-  if(!res) throw new Error("cachePersona() Failed to Fetch Persona from Chain:'"+chain+"' tokenId:'"+tokenId+"' contract:'"+contract+"'");
-  //Log
-  logger.warn("[TEST] cachePersona() Request Params: chain:'"+chain+"' tokenId:'"+tokenId+"' contract:'"+contract+"' Ret:" + JSON.stringify(ret) );
+  if(!chain || !contract || !tokenId) throw new Error("Missing Parameters -- Contract:"+contract+" Chain:"+chain+" TokenId:"+tokenId);
+  //Fetch Token Owner
+  let owner = await getTokenOwner(chain, contract, tokenId);
+  // let token_uri = await getTokenURI(chain, contract, tokenId);
+  
   //Register & return ParseObject
   let persona = new Persona(
     // {
@@ -61,68 +131,75 @@ const cachePersona = async (chain, contract, tokenId) => {
   await persona.save({
     chain,
     address: contract,
-    token_uri: tokenId,
+    token_id: tokenId.toString(),
+    // token_uri, 
+    owner, 
   }, {useMasterKey: true});
-
-  logger.warn("[TEST] cachePersona() Cached New Persona: chain:'"+chain+"' tokenId:'"+tokenId+"' contract:'"+contract+"' Persona:" + JSON.stringify(persona) );
+  // logger.warn("[i] cachePersona() Cached New Persona: chain:'"+chain+"' tokenId:'"+tokenId+"' contract:'"+contract+"' owner:'"+owner+"' Persona:" + JSON.stringify(persona) ); //V
   //Return new Persona Object
   return persona;
 };
 
-//-- TESTING
+//-- PRODUCTION
 
 /**
  * Register Persona to Network
  */
-Moralis.Cloud.define("personaRegister", async (request) => {  
+ Moralis.Cloud.define("personaRegister", async (request) => {  
   const { handle, contract, token_id, chain } = request.params;
-    
-  //Log
-  logger.warn("[TEST] personaRegister() Request Params: chain:'"+chain+"' token_id:'"+token_id+"' contract:'"+contract+"'");
-
   //Validate Parameters
-  if(!handle || !contract || !token_id || !chain) throw new Error("Missing Parameters (handle, contract, token_id, chain)");
+  if(!handle || !contract || !token_id || !chain) throw new Error("Missing Parameters (chain:'"+chain+"' token_id:'"+token_id+"' contract:'"+contract+"' handle:'"+handle+"')");
   //Validate Handle
   let isFree = await isHandleFree(handle);
   if(!isFree) throw new Error("Handle:'"+handle+"' is already owned");
+
+  //Log
+  logger.warn("[TEST] personaRegister() Request Params: chain:'"+chain+"' token_id:'"+token_id+"' contract:'"+contract+"' handle:'"+handle+"'");
+    
   //Get / Add Persona
   const query = new Moralis.Query(Persona);
   query.equalTo("chain", chain);
   query.equalTo("address", contract);
   query.equalTo("token_id", token_id);
   const results = await query.limit(1).find();
-
-
-  const personaGet = (res) => {
-    return res[0] ? res[0] : 'Empty';
-  }
-  logger.warn("[TEST] personaRegister() personaGet:"+personaGet(res));
-
-
-  if(results.length == 0){
+  //Extract Persona From Result or Make New
+  const personaGet = async (results) => {
+    if(results.length) return results[0];
     logger.warn("[TEST] personaRegister() Register Token to DB chain:'"+chain+"' token_id:'"+token_id+"' contract:'"+contract+"'");
-    let persona = await cachePersona(chain, contract, tokenId);
-    //Validate
-    if(!persona) throw new Error("Failed to Fetc Persona -- chain:'"+chain+"' token_id:'"+token_id+"' contract:'"+contract+"'")
-    persona.save({handle: handle.trim().toLowerCase()}, {useMasterKey: true});
-    // persona.save({handle: handle.trim().toLowerCase(), owner:request.user?.id}, {useMasterKey: true});
+    return await cachePersona(chain, contract, token_id);
+    // logger.warn("[TEST] personaRegister() Cached New Persona:" + JSON.stringify(persona) );
   }
-  else{
-    let persona = resutls[0];
-    // persona.set('handle', handle);
-    persona.save({handle: handle.trim().toLowerCase()}, {useMasterKey: true});
-    // persona.save({handle: handle.trim().toLowerCase(), owner:request.user?.id}, {useMasterKey: true});
-  }
-  
+  //Fetch Persona (Get or Make New)
+  let persona = await personaGet(results);
+  //Validate
+  if(!persona) throw new Error("Failed to Fetch Persona -- chain:'"+chain+"' token_id:'"+token_id+"' contract:'"+contract+"'")
+  //Add Handle to Persona
+  persona.save({handle: handle.trim().toLowerCase()}, {useMasterKey: true});
+  //Log  
+  logger.log("personaRegister() Saved Persona to Handle:'"+handle.trim().toLowerCase()+"'");
   //Log
-  logger.log("personaRegister() Registered Handle:'"+handle+"' to token_id:'"+token_id+"' chain:'"+chain+"' contract:'"+contract+"'");
+  // logger.warn("personaRegister() Registered Handle:'"+handle+"' to token_id:'"+token_id+"' chain:'"+chain+"' contract:'"+contract+"'");
+  return true;
 });
+
+
+//-- TESTING
 
 /**
  * Un-Register Persona from Network
  */
 Moralis.Cloud.define("personaUnregister", async (request) => { 
     const handle = request?.params?.handle;
+    if(!handle) throw new Error("Missing Handle");
+    const query = new Moralis.Query(Persona);
+    query.equalTo("handle", handle.trim().toLowerCase());
+    const results = await query.find();
+    if(!results.length) throw new Error("Handle:'"+handle+"' not found");
+    const persona = results[0];
+    //Remove Handle
+    persona.save({handle:null}, {useMasterKey: true});
+    //Return
+    return true;
 });
 
 /**
