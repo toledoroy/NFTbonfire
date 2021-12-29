c138565dbc33f5ffe3f0c1688c37d442b0ee1c0a
Roy <toledoroy@gmail.com>
2021-12-28 23:28:59 -0800

    Added Persona Provider Context

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { MoralisProvider } from "react-moralis";
 import "./index.css";
 import QuickStart from "components/QuickStart";
 import { MoralisDappProvider } from "./providers/MoralisDappProvider/MoralisDappProvider";
+import { PersonaContext } from "common/context";
 
 /** Get your free Moralis Account https://moralis.io/ */
 
@@ -12,15 +13,18 @@ const APP_ID = process.env.REACT_APP_MORALIS_APPLICATION_ID;
 const SERVER_URL = process.env.REACT_APP_MORALIS_SERVER_URL;
 
 const Application = () => {
+  const [ persona, setPersona ] = useState();
   const isServerInfo = APP_ID && SERVER_URL ? true : false;
   //Validate
   if(!APP_ID || !SERVER_URL) throw new Error("Missing Moralis Application ID or Server URL. Make sure to set your .env file.");
   if (isServerInfo)
     return (
       <MoralisProvider appId={APP_ID} serverUrl={SERVER_URL}>
-        <MoralisDappProvider>
-          <App isServerInfo />
-        </MoralisDappProvider>
+        <PersonaContext.Provider value={{persona, setPersona}}>
+          <MoralisDappProvider>
+            <App isServerInfo />
+          </MoralisDappProvider>
+        </PersonaContext.Provider>
       </MoralisProvider>
     );
   else {
