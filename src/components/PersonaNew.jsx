7c624b791da4f02e825af87c10eeb3b5b584f3c0
Roy <toledoroy@gmail.com>
2022-01-02 10:58:05 -0800

    Cleanup

--- src/components/PersonaNew.jsx
+++ src/components/PersonaNew.jsx
@@ -3,30 +3,11 @@ import { useMoralis, useMoralisWeb3Api } from "react-moralis";
 import PagePersona from "components/PagePersona";
 // import PageAuthenticate from "components/PageAuthenticate";
 
-// const { TabPane } = Tabs;
-// const { Panel } = Collapse;
-
-//Persona Fields Mapping
-// const personaFields = require('schema/PersonaData.json');
-
-//Persona Contract ABI
-// import personaABI from "contracts/abi/PERSONA.json";
-// const personaABI = require('contracts/abi/PERSONA.json');
-// console.log("PersonaNew() personaABI:", personaABI);
-// const contractPersona = Persona.getContractData();
-
 /**
  * Component - New Persona Controller (for Validation)
  */
 function PersonaNew(props) {
-    // const { handle, chain, contract, token_id } = params;
-    const { Moralis, isWeb3Enabled, isAuthenticated, setUserData, userError, user, chainId, account } = useMoralis();     //isUserUpdating
-    // const [ collection, setCollection ] = useState(null);
-    // const [ isEditMode, setIsEditMode ] = useState(false);
-    // const [ metadata, setMetadata ] = useState(null);
-    //File Upload
-    // const [ imageUrl, setImageUrl ] = useState(metadata?.image);
-    // const [ imageLoading, setImageLoading ] = useState(false);
+    const { isAuthenticated } = useMoralis();
 
     if(isAuthenticated) return PagePersona(props);
     else{
