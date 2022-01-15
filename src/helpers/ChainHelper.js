64c57fe383c52d357485b80724043eec4a807486
Roy <toledoroy@gmail.com>
2022-01-14 17:04:11 -0800

    [Review] Comments

--- src/helpers/ChainHelper.js
+++ src/helpers/ChainHelper.js
@@ -2,7 +2,12 @@ import ChainsData from "components/Chains/ChainsData";
 
 //Export NFT Helper
 export const ChainHelper = {
-    
+    /**
+     * Get Chain Data by Chain ID
+     * @param {*} chain 
+     * @param {*} key 
+     * @returns 
+     */
     get(chain, key){ return ChainsData[chain]?.[key]; }
 };
 
