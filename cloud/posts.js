82dd770db1c08dce99bb0dce95e5f23c1ea0d09e
Roy <toledoroy@gmail.com>
2022-01-21 12:31:32 -0800

    [FEAT] Added Parent Comment Count After Post Submission

--- cloud/posts.js
+++ cloud/posts.js
@@ -1,8 +1,88 @@
 //Objects
 const Post = Moralis.Object.extend("Post");
 
+//-- Production
+
+/**
+ * Calculate Entity's Score (Based on Relationship Votes)
+ * @param {*} entity 
+ */
+const postChildCount = async (postId) => {
+  
+  // const results = await query.limit(1).find();
+  const query = new Moralis.Query(Post);
+  query.equalTo("parentId", postId);
+  const count = await query.count({useMasterKey: true});
+  //Log
+  logger.warn("[TEST] postChildCount() entity:'"+postId+"' Count:"+count);
+
+  return count;
+};//postChildCount()
+
+
+/**
+ * DB - Custom Post Save Validation
+ * Make sure that author has access to Room
+ */
+ Moralis.Cloud.afterSave('Post', async request => {
+  if(request.master) return;
+  //Fetch Post Object
+  const object = request.object;
+  // logger.warn("[DEV] Post afterSave Hook W/Object: "+JSON.stringify(object));
+  // try{
+    const parentId = object.get('parentId');
+    // logger.warn("[TEST] Post afterSave Hook parentId:"+parentId);
+    if(parentId){
+      const count = await postChildCount(parentId);
+      // logger.warn("[TEST] Post afterSave Hook PostCount:"+count);
+      //Fetch Parent Post
+      let parentPost = await new Moralis.Query('Post').get(parentId, {useMasterKey: true});
+      //Update Parent Post
+      parentPost.save({childCount:count}, {useMasterKey: true});
+    }
+
+  // }
+  // catch(error){
+  //   logger.error("[CAUGHT] afterSave Hook: "+JSON.stringify(error));
+  // }
+});
+
 //-- DEV
 
+/**
+ * Post Subscription Management
+ * @param string postId
+ * @param bool action     [Optional] subscription action  [1-subscribe, 0-unsubscribe]
+ */
+Moralis.Cloud.define("postSubscribe", async (request) => {  
+  //Validate
+  if(!request?.user?.id) throw new Error("postSubscribe() Missing User ID:"+request?.user?.id+"'");
+
+  //Extract Parameters
+  const { postId, action } = request.params;
+  //Validate
+  if(!postId) throw new Error("Missing Request Parameters (postId)");
+  //Fetch Post 
+  let post = await new Moralis.Query('Post').get(postId, {useMasterKey: true});
+  //Validate
+  if(!post) throw new Error("postVote() Post Not Found: "+postId);
+
+  //TBD...
+
+  try{
+
+    //Log
+    // logger.warn("[TEST] post() by User:"+request?.user?.id);
+    // logger.warn(JSON.stringify(Moralis.User.current()));
+    // logger.warn(JSON.stringify(request?.user));
+    // logger.warn(JSON.stringify(data));
+  
+
+  }
+  catch(err){
+    logger.error("[CAUGHT] post() Error:"+err, {err, request});
+  }
+});
 
 /** UNUSED - THIS SHOUD MOVE TO CLIENTSIDE!
  * New Post
@@ -17,8 +97,6 @@ const Post = Moralis.Object.extend("Post");
       //Log
       // logger.warn("[TEST] data()", request.params);
       logger.warn("[TEST] post() by User:"+request?.user?.id);
-      logger.warn(request.params);
-      logger.warn(request.user);
       if(request?.user?.id){
         //TODO: Further validation - Get NFT & Validte User's Relation to NFT
   
