0faa0a1243fc9690fe84c01f1f87d653bc9c4fb9
Roy <toledoroy@gmail.com>
2022-02-14 18:12:22 -0800

    [Style] Personas Changed Card Buttons for Persona

--- src/components/Personas.jsx
+++ src/components/Personas.jsx
@@ -1,9 +1,13 @@
 import { useMoralis } from "react-moralis";
 // import { ChainHelper } from "helpers/ChainHelper";
-import NFTDisplaySingle from "components/NFTCollections/NFTDisplaySingle";
+// import NFTDisplaySingle from "components/NFTCollections/NFTDisplaySingle";
+import NFTDisplaySingle from "components/NFT/DisplaySinglePersona";
 import { useContractTokens } from "hooks/useContractTokens";
 // import PageAuthenticate from "components/PageAuthenticate";
 import { Skeleton } from 'antd';
+import { Persona } from "objects/Persona";
+// import { PersonaHelper } from "helpers/PersonaHelper";
+import { Link } from "react-router-dom";
 
 /**
  * Component: NFTs from All (Supported) Chains
@@ -11,26 +15,31 @@ import { Skeleton } from 'antd';
  * @returns 
  */
  function Personas(props){
-    const { account, isWeb3Enabled, isAuthenticated, chainId } = useMoralis();
+    const { isWeb3Enabled, isAuthenticated } = useMoralis();
 
-    const params = {
-            address: '0x9E91a8dDF365622771312bD859A2B0063097ad34',
-            chain: '0x4',
-        };
-    const { tokens, isLoading } = useContractTokens(params);
-    console.error("Personas:", tokens);
+    //Personas
+    const params1 = {address: Persona.getContractAddress('0x4'), chain: '0x4',};
+    const params2 = {address: Persona.getContractAddress('0xa869'), chain: '0xa869',};
+    const { tokens:tokens1, isLoading:isLoading1 } = useContractTokens(params1);
+    const { tokens:tokens2, isLoading:isLoading2 } = useContractTokens(params2);
+    // console.error("Personas:", {tokens1, tokens2});
 
     // if(!isWeb3Enabled || !isAuthenticated) return <PageAuthenticate />;
+    // <Link key={nft.token_address+'L'+nft.token_id} to={{pathname:"/personatoken/"+nft.chain+"/"+nft.token_address+"/"+nft.token_id}}> 
     return (
     <div className="framed">
-        <Skeleton loading={!isWeb3Enabled || !isAuthenticated || isLoading}>
 
-            <div key={params.hash+'items'} className="NFTitems" id={"NFTitems"+params.hash} style={props.style}> 
-                {tokens.map((nft, index) => (
+        <Skeleton loading={!isWeb3Enabled || !isAuthenticated || isLoading1 || isLoading2}>
+            <div key={params1.hash+'items'} className="NFTitems" id={"NFTitems"+params1.hash} style={props.style}> 
+                {tokens1.map((nft) => (
+                    <NFTDisplaySingle key={nft.token_address+':'+nft.token_id} nft={nft} />
+                ))}
+                {tokens2.map((nft) => (
                     <NFTDisplaySingle key={nft.token_address+':'+nft.token_id} nft={nft} />
                 ))}
             </div>
         </Skeleton>
+
     </div>
     );
 }
