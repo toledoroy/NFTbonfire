b326eda48d85c5798acd45bc314ee53d19ca3963
Roy <toledoroy@gmail.com>
2022-02-21 10:11:57 -0800

    Enable Rinkeby on chain changer

--- src/components/Chains/Chains.jsx
+++ src/components/Chains/Chains.jsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from "react";
 import { Menu, Dropdown, Button } from "antd";
 import { DownOutlined } from "@ant-design/icons";
-// import { AvaxLogo, PolygonLogo, BSCLogo, ETHLogo } from "./Logos";
-import { useChain } from "react-moralis";
-// import { getChainLogo } from "helpers/networks";    //Usage: getChainLogo("0x1")
 import { ChainHelper } from "helpers/ChainHelper";
+import { useChain } from "react-moralis";
+// import { AvaxLogo, PolygonLogo, BSCLogo, ETHLogo } from "./Logos";
+// import { getChainLogo } from "helpers/networks";    //Usage: getChainLogo("0x1")
 
 const styles = {
   item: {
@@ -16,12 +16,7 @@ const styles = {
     fontSize: "14px",
     padding: "0 10px",
   },
-  // button: {
-  //   border: "2px solid rgb(231, 234, 243)",
-  //   borderRadius: "12px",
-  // },
 };
-
 
 const allChains = ChainHelper.allChainsData();
 
@@ -33,8 +28,8 @@ function Chains() {
   const [selected, setSelected] = useState({});
   //Filter
   // const menuItems = allChains.filter((chainData) => (chainData.key == chainId || chainData.supported && (chainData.live || process?.env?.REACT_APP_ENV==='development')));
-  // const menuItems = allChains.filter((chainData) => (chainData.key === chainId || chainData.supported));   //This Allows Test Networks on Production
-  const menuItems = allChains.filter((chainData) => (chainData.key === chainId || chainData.key === '0xa869'));   //Avax Testnet Only
+  const menuItems = allChains.filter((chainData) => (chainData.key === chainId || chainData.supported));   //This Allows Test Networks on Production
+  // const menuItems = allChains.filter((chainData) => (chainData.key === chainId || chainData.key === '0xa869'));   //Avax Testnet Only
 
   // console.log("chain", chain);
 
@@ -52,7 +47,7 @@ function Chains() {
 
   const menu = menuItems.length < 2 ? '' : (
     <Menu onClick={handleMenuClick}>
-      {menuItems.map((item) => { 
+      {menuItems.map((item) => {
         return (
           <Menu.Item key={item.key} icon={item.icon} style={styles.item}>
             <span style={{ marginLeft: "5px" }}>{item.name}</span>
@@ -62,14 +57,14 @@ function Chains() {
     </Menu>
   );
 
-  let className = menuItems.length > 1 ? "chainSelect lightUp" :"chainSelect"; 
+  let className = menuItems.length > 1 ? "chainSelect lightUp" : "chainSelect";
   return (
     <div>
       <Dropdown overlay={menu} trigger={["click"]}>
         {/* <Button className="chainSelect" key={selected?.key} icon={selected?.icon} style={{ ...styles.button, ...styles.item }}> */}
         <Button className={className} key={selected?.key} icon={selected?.icon}>
           <span style={{ marginLeft: "5px" }}>{selected?.name}</span>
-          {menuItems.length>1 && <DownOutlined />}
+          {menuItems.length > 1 && <DownOutlined />}
         </Button>
       </Dropdown>
     </div>
